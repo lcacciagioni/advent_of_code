bb3748f1edd7ebb8a622b052639580cda94135bc
Leandro David Cacciagioni <leandro.21.2008@gmail.com>
2016-10-10 17:04:18 +0200

    Something more for 5

--- day5/1.exs
+++ day5/1.exs
@@ -13,9 +13,8 @@ defmodule NiceString do
     cl_line = line |> to_charlist |> Enum.filter(fn ch -> ch != ?\n end)
     vowels = check_vowels(cl_line)
     has_double = check_double(cl_line)
-    is_valid = check_invalid_pairs(cl_line)
+    is_valid = !String.contains? cl_line |> to_string, ["ab","cd","pq","xy"]
     if [vowels,has_double,is_valid] === [true,true,true] do
-      IO.puts line |> to_string
       count(tail, good_lines + 1)
     else
       count(tail, good_lines)
@@ -23,11 +22,12 @@ defmodule NiceString do
   end
 
   defp check_vowels(line) do
-    has_a = Enum.member?(line, ?a)
-    has_e = Enum.member?(line, ?e)
-    has_i = Enum.member?(line, ?i)
-    has_o = Enum.member?(line, ?o)
-    has_u = Enum.member?(line, ?u)
+    new_line = line |> to_string
+    has_a = String.contains? new_line, "a"
+    has_e = String.contains? new_line, "e"
+    has_i = String.contains? new_line, "i"
+    has_o = String.contains? new_line, "o"
+    has_u = String.contains? new_line, "u"
 
     if Enum.count([has_a,has_e,has_i,has_o,has_u], fn(x) -> x === true end) >= 3 do
       true
@@ -43,26 +43,10 @@ defmodule NiceString do
   defp check_double(line) do
     [first|tail] = line
     [second|_] = tail
-    if first == second do
+    if first === second do
       true
     else
       check_double(tail)
-    end
-  end
-
-  defp check_invalid_pairs([_]) do
-    true
-  end
-
-  defp check_invalid_pairs(line) do
-    [first|tail] = line
-    [second|_] = tail
-    case [first,second] do
-       [?a,?b] -> false
-       [?c,?d] -> false
-       [?p,?q] -> false
-       [?x,?y] -> false
-       _ -> check_invalid_pairs(tail)
     end
   end
 end
