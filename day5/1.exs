93534f72b3303249cbcbf27811111d12b386cc74
Leandro David Cacciagioni <leandro.21.2008@gmail.com>
2016-10-10 13:09:21 +0200

    5 so far

--- day5/1.exs
+++ day5/1.exs
@@ -1,10 +1,25 @@
 input = '5.txt' |> File.stream! |> Enum.to_list
 
-atoz="abcdefghijklmnopqrstuvwxyz"
-
 defmodule NiceString do
-  def find(lines) do
-    IO.puts "#{lines}"
+  def count(lines) do
+    count(lines, 0)
+  end
+
+  def count([], good_lines) do
+    good_lines
+  end
+
+  def count([line|tail], good_lines) do
+    cl_line = line |> to_charlist |> Enum.filter(fn ch -> ch != ?\n end)
+    vowels = check_vowels(cl_line)
+    has_double = check_double(cl_line)
+    is_valid = check_invalid_pairs(cl_line)
+    if [vowels,has_double,is_valid] === [true,true,true] do
+      IO.puts line |> to_string
+      count(tail, good_lines + 1)
+    else
+      count(tail, good_lines)
+    end
   end
 
   defp check_vowels(line) do
@@ -14,32 +29,42 @@ defmodule NiceString do
     has_o = Enum.member?(line, ?o)
     has_u = Enum.member?(line, ?u)
 
-    sum = sum_vowels([has_a, has_e, has_i, has_o, has_u], 0)
-    if sum >= 3 do
+    if Enum.count([has_a,has_e,has_i,has_o,has_u], fn(x) -> x === true end) >= 3 do
       true
     else
       false
     end
   end
 
-  defp sum_vowels([], sum) do
-    sum
-  end
-  defp sum_vowels([head|tail], sum) when head == true do
-    sum_vowels(tail, sum + 1)
-  end
-  defp sum_vowels([_|tail], sum) do
-    sum_vowels(tail, sum)
+  defp check_double([_]) do
+    false
   end
 
-  def check_double(_, []) do
-    IO.puts "Done!!!"
+  defp check_double(line) do
+    [first|tail] = line
+    [second|_] = tail
+    if first == second do
+      true
+    else
+      check_double(tail)
+    end
   end
 
-  def check_double(line, [head|tail]) do
-    IO.puts "#{line} | #{head}"
-    check_double(line, tail)
+  defp check_invalid_pairs([_]) do
+    true
+  end
+
+  defp check_invalid_pairs(line) do
+    [first|tail] = line
+    [second|_] = tail
+    case [first,second] do
+       [?a,?b] -> false
+       [?c,?d] -> false
+       [?p,?q] -> false
+       [?x,?y] -> false
+       _ -> check_invalid_pairs(tail)
+    end
   end
 end
 
-NiceString.check_double("hola",atoz)
+IO.puts NiceString.count(input) |> to_string
